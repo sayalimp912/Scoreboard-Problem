188a1435e89e4d657506503a50adad05a62da6fa
pendhasm <pendhasm@mail.uc.edu>
2016-04-15 21:16:41 -0400

    updated README and js file.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
+<snippet>
+  <content>
 # Scoreboard-Problem
 A Scoreboard page using HTML, CSS and JQuery.
+
+Give the names and scores of two teams in the given text boxes and find who the winner is!
+
+
+## Usage
+1. Open the file 'scoreboard.html' in your browser to view the web page.
+
+
+## Author
+
+Sayali Pendharkar
+
+mailto: sayali.mp912@gmail.com
+
+
+</content>
+</snippet>
+
--- scoreboard.js
+++ scoreboard.js
@@ -1,41 +1,49 @@
 $(document).ready(function(){
 	$('#submit_button').prop('disabled', true);
+	$(".result").text('');
 	// The Submit button is only enabled when all 4 input fields are entered.
     $('input').on("keyup",function() {
-    	team1 = $.trim($("#teamName1").val()).length;
-		team2 = $.trim($("#teamName2").val()).length;
-		score1 = $.trim($("#teamScore1").val()).length;
-		score2 = $.trim($("#teamScore2").val()).length;
-		if(team1 > 0 && team2 > 0 && score1 > 0 && score2 > 0){
+    	var team1 = $.trim($("#teamName1").val());
+		var team2 = $.trim($("#teamName2").val());
+		var score1 = parseInt($("#teamScore1").val());
+		var score2 = parseInt($("#teamScore2").val());
+		if(team1 != '' && team2 != '' && score1 >= 0 && score2 >= 0){
 			$('#submit_button').prop('disabled', false);
 		}
 	});
 	$("#submit_button").click(function(){
-		// display result
-		team1 = $.trim($("#teamName1").val()).length;
-		team2 = $.trim($("#teamName2").val()).length;
-		score1 = $.trim($("#teamScore1").val()).length;
-		score2 = $.trim($("#teamScore2").val()).length;
-		if(team1 > 0 && team2 > 0 && score1 > 0 && score2 > 0){
-			$('#submit_button').prop('disabled', false);
-			result_str = "And the winner is "		
-			if($("#teamScore1").val() == $("#teamScore2").val()){
-				result_str = 'It is a tie! The winners are <strong>' + $("#teamName1").val() + '</strong> and <strong>' + $("#teamName2").val() + '</strong> with a score of <strong>' + $("#teamScore1").val() + '</strong>'
-			}else if($("#teamScore1").val() > $("#teamScore2").val()){
-				result_str += '<strong>' + $("#teamName1").val() + '</strong> with a score of <strong>' + $("#teamScore1").val() + '</strong>'
+		var team1 = $.trim($("#teamName1").val());
+		var team2 = $.trim($("#teamName2").val());
+		var score1 = parseInt($("#teamScore1").val());
+		var score2 = parseInt($("#teamScore2").val());
+		if(team1 != '' && team2 != '' && score1 >= 0 && score2 >= 0){
+			// team names should not be the same
+			if(team1 != team2){
+				$('#submit_button').prop('disabled', false);
+				// Scores are shown at the bottom of the screen with the winner's name and score in bold
+				// In the case of a tie then illustrate this by bolding both teams and scores
+				result_str = "And the winner is ";		
+				if(score1 == score2){
+					result_str = 'It is a tie! The winners are <strong>' + team1 + '</strong> and <strong>' + team2 + '</strong> with a score of <strong>' + score1 + '</strong>!';
+				}else if(score1 > score2){
+					result_str += '<strong>' + team1 + '</strong> with a score of <strong>' + score1 + '</strong>!';
+				}else{
+					result_str += '<strong>' + team2 + '</strong> with a score of <strong>' + score2 + '<strong>!';
+				}
+				// display result
+				$("p.result").html(result_str);
+				// All 4 input fields are cleared upon submission of the score.
+				// Disable submit button upon submission of the score
+				$("#teamName1").val('');
+				$("#teamName2").val('');
+				$("#teamScore1").val('');
+				$("#teamScore2").val('');
+				$('#submit_button').prop('disabled', true);
 			}else{
-				result_str += '<strong>' + $("#teamName2").val() + '</strong> with a score of <strong>' + $("#teamScore2").val() + '<strong>'
+				alert("Please check your team names!")
 			}
-			$("p.result").html(result_str)
-			// All 4 input fields are cleared upon submission of the score.
-			// Disable submit button upon submission of the score
-			$("#teamName1").val('');
-			$("#teamName2").val('');
-			$("#teamScore1").val('');
-			$("#teamScore2").val('');
-			$('#submit_button').prop('disabled', true);
 		}else{
-			alert("Please fill all the fields.")
+			alert("Please fill all the fields.");
 		}
 		
 	});
